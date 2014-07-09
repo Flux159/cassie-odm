222541f590cace1406556f4d56d006a2ac1786b8
Suyog Sonwalkar <flux159@gmail.com>
2014-07-09 00:13:59 -0400

    Buffers work, just need to test maps now.

--- test/readme/mapsbuffer_test.js
+++ test/readme/mapsbuffer_test.js
@@ -10,13 +10,13 @@ describe('Types', function() {
         var config = {keyspace: "CassieTest", hosts: ["127.0.0.1:9042"]};
         cassie.connect(config);
 
-        //Testing everything but maps and arrays
-        var TypeSchema = new cassie.Schema({name: String, value: Number, time: Date, largeint: cassie.types.Long, int: cassie.types.Int, dbl: cassie.types.Double});
-        var Type = cassie.model('Type', TypeSchema);
+//        var MapsBuffer = new cassie.Schema({name: String, specialMap: {type: {String: String}, buff: cassie.types.Buffer}});
+        var MapsBuffer = new cassie.Schema({name: String, buff: Buffer});
+        var Maps = cassie.model('Maps', MapsBuffer);
 
         cassie.syncTables(config, {debug: true, prettyDebug: true}, function (err, results) {
 
-            var t1 = new Type({name: 'Hello', value: 3, time: new Date(), largeint: cassie.types.Long.fromString('12345'), int: 42, dbl: 5324.53});
+            var t1 = new Maps({name: 'Hello', buff: Buffer('dadfdjkva')});
 
             t1.save({debug: true, prettyDebug: true}, function(err) {
                 if(err){
@@ -25,7 +25,10 @@ describe('Types', function() {
                     done();
                 } else {
 
-                    Type.find({id: t1.id}, function(err, results) {
+                    Maps.find({id: t1.id}, function(err, results) {
+
+                        console.log(results.toString());
+
                         cassie.close();
                         done();
                     });
