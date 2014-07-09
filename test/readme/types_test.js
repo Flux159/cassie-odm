2b1b8bfb07c939e070d2bf769b1b65127ff86b1c
Suyog Sonwalkar <flux159@gmail.com>
2014-07-09 00:04:13 -0400

    Numbers, Ints, and Doubles seem to work. Just need to test out maps now.

--- test/readme/types_test.js
+++ test/readme/types_test.js
@@ -11,14 +11,12 @@ describe('Types', function() {
         cassie.connect(config);
 
         //Testing everything but maps and arrays
-        var TypeSchema = new cassie.Schema({name: String, value: Number, time: Date, largeint: cassie.types.Long});
-//        var TypeSchema = new cassie.Schema({name: String, value: Number, time: Date});
+        var TypeSchema = new cassie.Schema({name: String, value: Number, time: Date, largeint: cassie.types.Long, int: cassie.types.Int, dbl: cassie.types.Double});
         var Type = cassie.model('Type', TypeSchema);
-
 
         cassie.syncTables(config, {debug: true, prettyDebug: true}, function (err, results) {
 
-            var t1 = new Type({name: 'Hello', value: 3, time: new Date(), largeint: cassie.types.Long.fromString('12345')});
+            var t1 = new Type({name: 'Hello', value: 3, time: new Date(), largeint: cassie.types.Long.fromString('12345'), int: 42, dbl: 5324.53});
 
             t1.save({debug: true, prettyDebug: true}, function(err) {
                 if(err){
@@ -39,15 +37,6 @@ describe('Types', function() {
                 }
 
             });
-
-//            var kitty = new Cat({ name: 'Eevee'});
-//            kitty.save(function (err) {
-//                if (err) return console.log(err);
-//
-//                cassie.close();
-//                done();
-//
-//            });
 
         });
 
