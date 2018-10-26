32914eef8096dac041d04b7c6a63a898e199b3a3
Brad van der Laan <brad.vanderlaan@gmail.com>
2018-10-25 20:53:28 -0400

    :white_check_mark:Add tests for Long.fromString

--- test/types.unit.js
+++ test/types.unit.js
@@ -23,5 +23,25 @@ describe('Unit :: Types', function() {
         assert.equal(actual, '-1');
       });
     });
+
+    describe('fromString', function() {
+      it('should throw if not a string', function() {
+        assert.throws(function() {
+          types.datatypes.Long.fromString(123456789);
+        }, /Expected String/);
+      });
+
+      it('should convert string to complete actual value', function() {
+        // Storing this number in a Number would result in 9223372036854776000
+        // Using Long.fromString will ensure we get 9223372036854775807
+        var actual = types.datatypes.Long.fromString('9223372036854775807');
+        assert.equal(actual, 9223372036854775807);
+      });
+
+      it('should return negative value if exceeds twos-complement 64-bits', function() {
+        var actual = types.datatypes.Long.fromString('9223372036854775807009');
+        assert.equal(actual, '-991');
+      });
+    });
   });
 });
